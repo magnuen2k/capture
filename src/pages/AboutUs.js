1cab8c3152cc51974536bbffc4e3afcae2af08a2
magnuen2k <maen030@student.kristiania.no>
2020-11-19 00:47:25 +0100

    implement faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,16 @@
 import React from "react";
 // Page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
     return(
-        <div>
+        <>
             <AboutSection/>
-        </div>
+            <ServicesSection/>
+            <FaqSection/>
+        </>
     )
 }
 
