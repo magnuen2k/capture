af5d54969229a5c91d1f065d931cd44fab2b134e
magnuen2k <maen030@student.kristiania.no>
2020-11-19 00:04:21 +0100

    create about section and page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+// Import pages
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <p>TEST</p>
+      <AboutUs/>
     </div>
   );
 }
