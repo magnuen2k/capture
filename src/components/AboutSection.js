93335515b1880f7e4b15498c4b2c66e8b3517e0f
magnuen2k <maen030@student.kristiania.no>
2020-11-19 01:23:16 +0100

    adding global styling with styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from "react";
 import home1 from "../img/home1.png";
+// Styled
+import styled from "styled-components";
 
 const AboutSection = () => {
     return (
-        <div>
+        <About>
             <div className="description">
                 <div className="title">
                     <div className="hide">
@@ -23,8 +25,18 @@ const AboutSection = () => {
             <div className="image">
                 <img src={home1} alt="guy with camera"/>
             </div>
-        </div>
+        </About>
     )
 }
+
+// Styled components
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`
 
 export default AboutSection;
