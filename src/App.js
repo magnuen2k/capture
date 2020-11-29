d5eb1d305437532bc0637e7eeb661a5722d32e2d
magnuen2k <maen030@student.kristiania.no>
2020-11-29 16:46:24 +0100

    adding state to handle movie pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
 // router
 import {Switch, Route} from "react-router-dom";
 
@@ -19,8 +20,12 @@ function App() {
                 <AboutUs/>
             </Route>
 
-            <Route path="/work">
+            <Route path="/work" exact>
                 <OurWork/>
+            </Route>
+
+            <Route path="/work/:id">
+                <MovieDetail/>
             </Route>
 
             <Route path="/contact">
